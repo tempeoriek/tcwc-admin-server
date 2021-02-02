136d8a9c3216e8c97d6e7878b344ca2878f8741c
tempeoriek <orielaldo@gmail.com>
2021-02-02 13:53:15 +0700

    [BUG] chg get to post login tracker

--- api/routes/Logintracker.js
+++ api/routes/Logintracker.js
@@ -1,6 +1,6 @@
 const router = express.Router(),
 Logintracker = require('../controllers/LogintrackerController');
 
-router.get('/', Logintracker.getData);
+router.post('/', Logintracker.getData);
 
 module.exports = router;
